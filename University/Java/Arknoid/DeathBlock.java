32703ce5703112d62ec320255522853c1d421790
AmnonTamsut <67242604+AmnonTamsut@users.noreply.github.com>
2020-08-09 19:34:47 +0300

    Add files via upload

---
+++ University/Java/Arknoid/DeathBlock.java
@@ -0,0 +1,22 @@
+//ID:203215215
+
+import java.awt.Color;
+
+/**
+ * DeathBlock - a block that kills balls.
+ */
+public class DeathBlock extends Block {
+    /**
+     * DeathBlock
+     * <p>
+     * constructor.
+     *
+     * @param block - a given block
+     * @param color - a given color
+     */
+
+    public DeathBlock(Rectangle block, Color color) {
+        super(block, color);
+    }
+
+}
